e7a699b5ef7a35878a423d090f011e0169f20ac3
Max <max@protoplanetary.net>
2015-08-09 20:28:57 -0500

    Initial sketching and dev setup

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,10 +1,15 @@
 var gulp = require('gulp'),
   sass = require('gulp-sass'),
-  swig = require('gulp-swig');
+  swig = require('gulp-swig'),
+  webserver = require('gulp-webserver');
 
 gulp.task('html', [], function () {
   return gulp.src('src/html/*.html')
-    .pipe(swig())
+    .pipe(swig({
+      defaults: {
+        cache: false
+      }
+    }))
     .pipe(gulp.dest('build'));
 });
 
@@ -12,6 +17,16 @@ gulp.task('sass', [], function () {
   return gulp.src('src/scss/**/*')
     .pipe(sass())
     .pipe(gulp.dest('build/css'));
+});
+
+gulp.task('watch', function () {
+  return gulp.watch('src/**/*', ['build']);
+});
+
+gulp.task('dev', ['build', 'watch'], function () {
+  return gulp
+    .src('build')
+    .pipe(webserver());
 });
 
 gulp.task('build', ['html', 'sass']);
